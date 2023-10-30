bb41b837fb9075ffdbcc8bc2fbadd325fe5ac0d0
Arvin5G <arvingardon5@gmail.com>
2023-10-31 07:35:08 +0800

    testing

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -2,7 +2,7 @@
 @section('main-content')
 <div class="page-row row">
     <div class="col-xl-6">
-        <h2 class="page-heading">User Profile</h2>
+        <h2 class="page-heading">User test</h2>
     </div>
 </div>
 
